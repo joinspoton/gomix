fbfa47c143f74ed263eb95d0668805f97825f379
Yash Saxena <yash.saxena1217@gmail.com>
2018-08-01 13:22:41 -0700

    fix the parse example pacakge

--- gomix.go
+++ gomix.go
@@ -1,14 +1,6 @@
 package gomix
 
-import (
-	"fmt"
-)
-
 // TestingHere - First test helper function
 func TestingHere() string {
 	return "TESTING HERER"
-}
-
-func main() {
-	fmt.Println("TEST PACAKGE HERER")
 }
