db4183b6b5482eec4a465c5ad6354d041a674c69
Smati Tungkitkancharoen <smati@stanza.co>
2018-08-29 12:25:44 -0700

    Add args to connect

--- postgres/setup/main.go
+++ postgres/setup/main.go
@@ -5,6 +5,9 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
+func Connect(host string, port string, db string, username string, password string) {
+}
+
 func main() {
 	db, err := gorm.Open("postgres", "host=myhost port=myport user=gorm dbname=gorm password=mypassword")
 	defer db.Close()
