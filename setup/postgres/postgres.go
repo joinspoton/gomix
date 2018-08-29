24973481155101022d8cb577ac123d382462f066
Smati Tungkitkancharoen <smati@stanza.co>
2018-08-29 14:47:54 -0700

    Remove unused dialect. Change port to string

--- setup/postgres/postgres.go
+++ setup/postgres/postgres.go
@@ -4,12 +4,11 @@ import (
 	"fmt"
 
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 // Connect - Create a new PostGreSQL connection
-func Connect(host string, port int, db string, username string, password string) (*gorm.DB, error) {
-	connection := fmt.Sprintf("host=%s port=%d dbname=%s user=%s password=%s", host, port, db, username, password)
+func Connect(host string, port string, db string, username string, password string) (*gorm.DB, error) {
+	connection := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s", host, port, db, username, password)
 	client, err := gorm.Open("postgres", connection)
 	if err != nil {
 		return nil, fmt.Errorf("Unable to connect to postgres with: %+v", connection)
