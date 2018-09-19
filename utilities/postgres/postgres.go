31604b8b2667bf39054f94f1491576d490c115ce
Smati Tungkitkancharoen <smati@stanza.co>
2018-09-18 17:30:47 -0700

    Add skeletons

--- utilities/postgres/postgres.go
+++ utilities/postgres/postgres.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"fmt"
 	"strings"
+	"sync"
 
 	"github.com/jinzhu/gorm"
 )
@@ -10,6 +11,7 @@ import (
 // BatchInsert - Since it is not officially supported in the GORM API
 func BatchInsert(db *gorm.DB, table string, data []map[string]interface{}) {
 	batchSize := 500
+	batches := len(data)/batchSize + 1
 
 	columns := make([]string, len(data))
 	i := 0
@@ -23,6 +25,30 @@ func BatchInsert(db *gorm.DB, table string, data []map[string]interface{}) {
 		table,
 		strings.Join(columns, ","),
 	)
+
+	var wg sync.WaitGroup
+	wg.Add(batches)
+
+	for i := 0; i < len(data); i += 500 * len(headers) {
+
+		go func(i int) {
+			defer wg.Done()
+			stop := i + 500*len(headers)
+			subset := data[i:stop]
+			if stop > len(data) {
+				subset = data[i:len(data)]
+			}
+			postgresUtils.BatchInsert(
+				postgresClient,
+				"influx_bids2",
+				headers,
+				subset,
+			)
+		}(i)
+
+	}
+
+	wg.Wait()
 
 	var placeholders []string
 	for i := 0; i < len(columns); i++ {
