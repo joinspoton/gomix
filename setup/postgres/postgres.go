b2cdedef4286a8bda190da0690843fccf326c687
Smati Tungkitkancharoen <smati@stanza.co>
2018-09-20 14:05:25 -0700

    Move postgres utilities to setup for easy organization

--- setup/postgres/postgres.go
+++ setup/postgres/postgres.go
@@ -2,6 +2,8 @@ package postgres
 
 import (
 	"fmt"
+	"strings"
+	"sync"
 
 	"github.com/jinzhu/gorm"
 )
@@ -14,4 +16,58 @@ func Connect(host string, port string, db string, username string, password stri
 		return nil, fmt.Errorf("Unable to connect to postgres with: %+v", connection)
 	}
 	return client, nil
+}
+
+// BatchInsert - Since it is not officially supported in the GORM API
+func BatchInsert(db *gorm.DB, table string, data []map[string]interface{}) {
+	batchSize := 500
+	var batches [][]map[string]interface{}
+	for batchSize < len(data) {
+		data, batches = data[batchSize:], append(batches, data[0:batchSize:batchSize])
+	}
+	batches = append(batches, data)
+
+	var columns []string
+	for k := range data[0] {
+		columns = append(columns, k)
+	}
+
+	var placeholders []string
+	for i := 0; i < len(columns); i++ {
+		placeholders = append(placeholders, "?")
+	}
+	row := fmt.Sprintf("(%s)", strings.Join(placeholders, ","))
+
+	cmd := fmt.Sprintf(
+		"INSERT INTO %s(%s) VALUES ",
+		table,
+		strings.Join(columns, ","),
+	)
+
+	var wg sync.WaitGroup
+	wg.Add(len(batches))
+
+	for i := 0; i < len(batches); i++ {
+		go func(i int) {
+			defer wg.Done()
+
+			var inserts []string
+			for j := 0; j < len(batches[i]); j++ {
+				inserts = append(inserts, row)
+			}
+
+			query := cmd + strings.Join(inserts, ",")
+
+			var values []interface{}
+			for _, entry := range batches[i] {
+				for _, column := range columns {
+					values = append(values, entry[column])
+				}
+			}
+
+			db.Exec(query, values...)
+		}(i)
+	}
+
+	wg.Wait()
 }
