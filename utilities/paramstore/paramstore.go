ee25f4d3e59b883b9f19f75ca98f2aed465c51fa
Smati Tungkitkancharoen <smati@stanza.co>
2018-09-25 15:00:41 -0700

    Remove debugs

--- utilities/paramstore/paramstore.go
+++ utilities/paramstore/paramstore.go
@@ -19,38 +19,17 @@ func GetConfig(key string) (string, error) {
 		return "", err
 	}
 
-	fmt.Println("key", key)
-	c, _ := sess.Config.Credentials.Get()
-	fmt.Println("sess.Config.Credentials.Get()", c)
-
-	// fmt.Println("Trying to request...")
-	// resp, err := http.Get("https://github.com/")
-	// if err != nil {
-	// 	fmt.Println("Error!")
-	// }
-	// fmt.Println("After requesting...")
-	// defer resp.Body.Close()
-	// body, err := ioutil.ReadAll(resp.Body)
-	// fmt.Println("body", string(body))
-
 	ssmsvc := ssm.New(sess, aws.NewConfig().WithRegion("us-west-2"))
-	fmt.Printf("ssmsvc.ClientInfo: \n%+v\n", ssmsvc.ClientInfo)
-	d, _ := ssmsvc.Config.Credentials.Get()
-	fmt.Println("ssmsvc.Config.Credentials.Get()", d)
 	keyname := key
 	withDecryption := false
-	fmt.Println("Before ssmsvc.GetParameter()")
 	param, err := ssmsvc.GetParameter(&ssm.GetParameterInput{
 		Name:           &keyname,
 		WithDecryption: &withDecryption,
 	})
-	fmt.Println("After ssmsvc.GetParameter()")
-	fmt.Println("param.GoString()", param.GoString())
 	if err != nil {
-		fmt.Println("err", err)
+		fmt.Println("error: ", err)
+		return "", err
 	}
-
 	value := *param.Parameter.Value
-	fmt.Println("value", value)
 	return value, nil
 }
