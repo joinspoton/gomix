449e2853867ee68caedbf9753523c640ea92fa34
Mike Yin <yinjitong@hotmail.com>
2018-10-19 16:47:08 -0700

    mongo

---
+++ setup/mongo/mongo.go
@@ -0,0 +1,29 @@
+package postgres
+
+import (
+	"fmt"
+
+	"github.com/globalsign/mgo"
+)
+
+// ConnectToDB - Create a new mongo connection
+func ConnectToDB(server string, port string, db string, username string, password string) (*mgo.Database, error) {
+	session, err := mgo.Dial(server)
+	var credentials mgo.Credential
+	credentials.Username = username
+	credentials.Password = password
+	err = session.Login(&credentials)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Session created")
+	}
+	c := session.DB("calendar")
+	return c, nil
+}
+
+// ConnectToCollection - Create a new collection connection
+func ConnectToCollection(db *mgo.Database, collection string) (*mgo.Collection, error) {
+	c := db.C(collection)
+	return c, nil
+}
