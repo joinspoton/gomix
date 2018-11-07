d4e0b987ffa03f04861581792aab9c4fe9c887ff
Smati Tungkitkancharoen <smati@stanza.co>
2018-11-07 15:28:05 -0800

    Update mongo

--- setup/mongo/mongo.go
+++ setup/mongo/mongo.go
@@ -2,13 +2,32 @@ package mongo
 
 import (
 	"fmt"
+	"gomix/utilities/paramstore"
+	"gomix/utilities/system"
 	"strings"
 
 	"github.com/globalsign/mgo"
 )
 
-// ConnectToDB - Create a new mongo connection
-func ConnectToDB(replicaSet []map[string]string, db string, username string, password string) (*mgo.Database, error) {
+// Connect() - Create a new mongo connection without specifying credentials
+func Connect(database string) {
+	path := fmt.Sprintf("/%s/db/", system.GetEnv("stage", "staging"))
+
+	name, _ := paramstore.GetConfig(path + "name")
+	replicaSet, _ := paramstore.GetJSONArrayConfig(path + "replicaSetArray")
+	username, _ := paramstore.GetConfig(path + "username")
+	password, _ := paramstore.GetConfig(path + "password")
+
+	return ManuallyConnect(
+		replicaSet,
+		database,
+		username,
+		password
+	)
+}
+
+// ManuallyConnect - Create a new mongo connection
+func ManuallyConnect(replicaSet []map[string]string, db string, username string, password string) (*mgo.Database, error) {
 	var hostsArray []string
 	for _, replica := range replicaSet {
 		hostsArray = append(hostsArray, replica["host"]+":"+replica["port"])
