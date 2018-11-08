662db78a808574d51d45a43d182a00b68e11d383
Smati Tungkitkancharoen <smati@stanza.co>
2018-11-07 17:05:24 -0800

    Update influx

--- setup/influx/influx.go
+++ setup/influx/influx.go
@@ -3,6 +3,8 @@ package influx
 import (
 	"errors"
 	"fmt"
+	"gomix/utilities/paramstore"
+	"gomix/utilities/system"
 
 	"github.com/influxdata/influxdb/client/v2"
 )
@@ -17,8 +19,24 @@ type ConfigType struct {
 // ErrInfluxConnection - Error thrown when connection failes
 var ErrInfluxConnection = errors.New("INFLUX_FAILED_TO_CONNECT")
 
-// Connect - Create a new InfluxDB connection
-func Connect(host string, username string, password string) (clnt client.Client, err error) {
+// Connect - Create a new InfluxDB connection without credentials
+func Connect() (clnt client.Client, err error) {
+	path := fmt.Sprintf("/%s/influx/", system.GetEnv("stage", "staging"))
+
+	host, _ := paramstore.GetConfig(path + "host")
+	port, _ := paramstore.GetConfig(path + "port")
+	username, _ := paramstore.GetConfig(path + "username")
+	password, _ := paramstore.GetConfig(path + "password")
+
+	return ManuallyConnect(
+		fmt.Sprintf("http://%s:%s", host, port),
+		username,
+		password,
+	)
+}
+
+// ManuallyConnect - Create a new InfluxDB connection with credentials
+func ManuallyConnect(host string, username string, password string) (clnt client.Client, err error) {
 	c, err := client.NewHTTPClient(client.HTTPConfig{
 		Addr:     host,
 		Username: username,
