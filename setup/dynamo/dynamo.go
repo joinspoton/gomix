3d434a8697d4af3d6258e54e11af1c644dc3fe9d
Smati Tungkitkancharoen <smati@stanza.co>
2018-12-26 17:14:51 -0800

    Get client

--- setup/dynamo/dynamo.go
+++ setup/dynamo/dynamo.go
@@ -1,1 +1,16 @@
 package dynamo
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func getClient() *dynamodb.DynamoDB {
+	sess, _ := session.NewSession(&aws.Config{
+		Region: aws.String("us-west-2")},
+	)
+
+	// Create DynamoDB client
+	return dynamodb.New(sess)
+}
