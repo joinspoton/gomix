fb0d93a35a65dd2406590c1081dec12dccadcf8b
Smati Tungkitkancharoen <smati@stanza.co>
2019-01-10 15:06:28 -0800

    Remove stamp

--- structs/elasticsearch/event/event.go
+++ structs/elasticsearch/event/event.go
@@ -4,15 +4,15 @@ import "time"
 
 // Event - Structure and standardize raw events from different vendors
 type Event struct {
-	Vendor         string
-	Name           string
-	Description    string
-	StartTimestamp time.Time
-	EndTimestamp   time.Time
-	Price          float64
-	Currency       string
-	Longitude      float64
-	Latitude       float64
-	City           string
-	Country        string
+	Vendor      string
+	Name        string
+	Description string
+	StartTime   time.Time
+	EndTime     time.Time
+	Price       float64
+	Currency    string
+	Longitude   float64
+	Latitude    float64
+	City        string
+	Country     string
 }
