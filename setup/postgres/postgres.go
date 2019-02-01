bccd05a815363c00889e5d4ce06a37568a279756
Smati Tungkitkancharoen <smati@stanza.co>
2019-02-01 14:59:32 -0800

    Start batching struct inserts

--- setup/postgres/postgres.go
+++ setup/postgres/postgres.go
@@ -143,44 +143,53 @@ func BatchStructsInsert(db *gorm.DB, table string, objArr []interface{}) {
 		return
 	}
 
-	mainObj := objArr[0]
-	mainScope := db.NewScope(mainObj)
-	mainFields := mainScope.Fields()
-	quoted := make([]string, 0, len(mainFields))
-	for i := range mainFields {
-		// If primary key has blank value (0 for int, "" for string, nil for interface ...), skip it.
-		// If field is ignore field, skip it.
-		if (mainFields[i].IsPrimaryKey && mainFields[i].IsBlank) || (mainFields[i].IsIgnored) {
-			continue
-		}
-		quoted = append(quoted, mainScope.Quote(mainFields[i].DBName))
+	batchSize := 500
+	var batches [][]interface{}
+	for batchSize < len(objArr) {
+		objArr, batches = objArr[batchSize:], append(batches, objArr[0:batchSize:batchSize])
 	}
+	batches = append(batches, objArr)
 
-	placeholdersArr := make([]string, 0, len(objArr))
-
-	for _, obj := range objArr {
-		scope := db.NewScope(obj)
-		fields := scope.Fields()
-		placeholders := make([]string, 0, len(fields))
-		for i := range fields {
-			if (fields[i].IsPrimaryKey && fields[i].IsBlank) || (fields[i].IsIgnored) {
+	for _, batch := range batches {
+		mainObj := batch[0]
+		mainScope := db.NewScope(mainObj)
+		mainFields := mainScope.Fields()
+		quoted := make([]string, 0, len(mainFields))
+		for i := range mainFields {
+			// If primary key has blank value (0 for int, "" for string, nil for interface ...), skip it.
+			// If field is ignore field, skip it.
+			if (mainFields[i].IsPrimaryKey && mainFields[i].IsBlank) || (mainFields[i].IsIgnored) {
 				continue
 			}
-			placeholders = append(placeholders, mainScope.AddToVars(fields[i].Field.Interface()))
+			quoted = append(quoted, mainScope.Quote(mainFields[i].DBName))
 		}
-		placeholdersStr := "(" + strings.Join(placeholders, ", ") + ")"
-		placeholdersArr = append(placeholdersArr, placeholdersStr)
-		// add real variables for the replacement of placeholders' '?' letter later.
-		mainScope.SQLVars = append(mainScope.SQLVars, scope.SQLVars...)
-	}
 
-	mainScope.Raw(fmt.Sprintf("INSERT INTO %s (%s) VALUES %s",
-		table,
-		strings.Join(quoted, ", "),
-		strings.Join(placeholdersArr, ", "),
-	))
+		placeholdersArr := make([]string, 0, len(batch))
 
-	if _, err := mainScope.SQLDB().Exec(mainScope.SQL, mainScope.SQLVars...); err != nil {
-		panic(err)
+		for _, obj := range batch {
+			scope := db.NewScope(obj)
+			fields := scope.Fields()
+			placeholders := make([]string, 0, len(fields))
+			for i := range fields {
+				if (fields[i].IsPrimaryKey && fields[i].IsBlank) || (fields[i].IsIgnored) {
+					continue
+				}
+				placeholders = append(placeholders, mainScope.AddToVars(fields[i].Field.Interface()))
+			}
+			placeholdersStr := "(" + strings.Join(placeholders, ", ") + ")"
+			placeholdersArr = append(placeholdersArr, placeholdersStr)
+			// add real variables for the replacement of placeholders' '?' letter later.
+			mainScope.SQLVars = append(mainScope.SQLVars, scope.SQLVars...)
+		}
+
+		mainScope.Raw(fmt.Sprintf("INSERT INTO %s (%s) VALUES %s",
+			table,
+			strings.Join(quoted, ", "),
+			strings.Join(placeholdersArr, ", "),
+		))
+
+		if _, err := mainScope.SQLDB().Exec(mainScope.SQL, mainScope.SQLVars...); err != nil {
+			panic(err)
+		}
 	}
 }
