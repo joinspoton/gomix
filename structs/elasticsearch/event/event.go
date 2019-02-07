873c37e8fa55efbab000afd4af0aa50efbb1deee
Smati Tungkitkancharoen <smati@stanza.co>
2019-02-06 17:14:16 -0800

    Remove unique index for columns

--- structs/elasticsearch/event/event.go
+++ structs/elasticsearch/event/event.go
@@ -4,17 +4,17 @@ import "time"
 
 // Event - Structure and standardize raw events from different vendors
 type Event struct {
-	Vendor      string `gorm:"unique_index:all_columns"`
-	Name        string `gorm:"unique_index:all_columns"`
-	Description string `gorm:"unique_index:all_columns"`
-	StartTime   time.Time `gorm:"unique_index:all_columns"`
-	EndTime     time.Time `gorm:"unique_index:all_columns"`
-	PriceMin    float64 `gorm:"unique_index:all_columns"`
-	PriceMax    float64 `gorm:"unique_index:all_columns"`
-	Currency    string `gorm:"unique_index:all_columns"`
-	Address     string `gorm:"unique_index:all_columns"`
-	Longitude   float64 `gorm:"unique_index:all_columns"`
-	Latitude    float64 `gorm:"unique_index:all_columns"`
-	City        string `gorm:"unique_index:all_columns"`
-	Country     string `gorm:"unique_index:all_columns"`
+	Vendor      string
+	Name        string
+	Description string
+	StartTime   time.Time
+	EndTime     time.Time
+	PriceMin    float64
+	PriceMax    float64
+	Currency    string
+	Address     string
+	Longitude   float64
+	Latitude    float64
+	City        string
+	Country     string
 }
