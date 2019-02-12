a37ba9f9ea60fef874891e9de839ba4762ece0da
Smati Tungkitkancharoen <smati@stanza.co>
2019-02-12 10:47:00 -0800

    Remove MD5Description

--- structs/elasticsearch/event/event.go
+++ structs/elasticsearch/event/event.go
@@ -4,18 +4,17 @@ import "time"
 
 // Event - Structure and standardize raw events from different vendors
 type Event struct {
-	Vendor         string
-	Name           string
-	Description    string
-	Md5Description int
-	StartTime      time.Time
-	EndTime        time.Time
-	PriceMin       float64
-	PriceMax       float64
-	Currency       string
-	Address        string
-	Longitude      float64
-	Latitude       float64
-	City           string
-	Country        string
+	Vendor      string
+	Name        string
+	Description string
+	StartTime   time.Time
+	EndTime     time.Time
+	PriceMin    float64
+	PriceMax    float64
+	Currency    string
+	Address     string
+	Longitude   float64
+	Latitude    float64
+	City        string
+	Country     string
 }
