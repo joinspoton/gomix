b3a3fc3336e77b2230e8c26dbe43bd638c2d4db9
Arthur Kam <arthur@stanza.co>
2019-06-14 16:30:11 -0700

    a

--- structs/elasticsearch/event/event.go
+++ structs/elasticsearch/event/event.go
@@ -7,25 +7,27 @@ import (
 
 // Event - Structure and standardize raw events from different vendors
 type Event struct {
-	Vendor      string
-	Name        string
-	Description string
-	StartTime   time.Time
-	EndTime     time.Time
-	PriceMin    float64
-	PriceMax    float64
-	Currency    string
-	Address     string
-	Longitude   float64
-	Latitude    float64
-	City        string
-	Country     string
-	Tags        string
-	ImageURL    string
+	TicketmasterID string
+	Vendor         string
+	Name           string
+	Description    string
+	StartTime      time.Time
+	EndTime        time.Time
+	PriceMin       float64
+	PriceMax       float64
+	Currency       string
+	Address        string
+	Longitude      float64
+	Latitude       float64
+	City           string
+	Country        string
+	Tags           string
+	ImageURL       string
 }
 
 // AddDefaultValues - Give the event default values
 func AddDefaultValues(event *Event) {
+	event.TicketmasterID = ""
 	event.Vendor = ""
 	event.Name = ""
 	event.Description = ""
